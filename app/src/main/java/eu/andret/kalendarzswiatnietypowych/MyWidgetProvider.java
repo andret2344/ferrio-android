220364e1375b6740a411f6743667e0e678525230
Andret2344 <andret2344@gmail.com>
2021-11-04 18:56:35 +0100

    Enhanced widget provider code

--- app/src/main/java/eu/andret/kalendarzswiatnietypowych/MyWidgetProvider.java
+++ app/src/main/java/eu/andret/kalendarzswiatnietypowych/MyWidgetProvider.java
@@ -23,14 +23,20 @@ public class MyWidgetProvider extends AppWidgetProvider {
 	public void onUpdate(final Context context, final AppWidgetManager appWidgetManager, final int[] appWidgetIds) {
 		super.onUpdate(context, appWidgetManager, appWidgetIds);
 		final boolean dark = Data.getPreferences(context, Data.Prefs.THEME).getString(context.getResources().getString(R.string.settings_theme_widgets), "1").equals("1");
+
+		final RemoteViews remoteViews = new RemoteViews(context.getPackageName(), dark ? R.layout.widget_dark : R.layout.widget_light);
+		remoteViews.setTextViewText(R.id.widget_text_holiday, getContent(context));
+
 		final LocalDate now = LocalDate.now();
-		final int day = now.getDayOfMonth();
-		final int month = now.getMonthValue();
 		final Intent intent = new Intent(context, MainActivity.class);
 		intent.putExtra(MainActivity.FROM, MainActivity.WIDGET);
-		intent.putExtra(MainActivity.DAY, day);
-		intent.putExtra(MainActivity.MONTH, month);
-		final RemoteViews remoteViews = new RemoteViews(context.getPackageName(), dark ? R.layout.widget_dark : R.layout.widget_light);
+		intent.putExtra(MainActivity.DAY, now.getDayOfMonth());
+		intent.putExtra(MainActivity.MONTH, now.getMonthValue());
+		final PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+		remoteViews.setOnClickPendingIntent(R.id.widget_relative_main, pendingIntent);
+	}
+
+	private String getContent(final Context context) {
 		final SharedPreferences theme = Data.getPreferences(context, Data.Prefs.THEME);
 		final SharedPreferences prefs = Data.getPreferences(context, Data.Prefs.LANGUAGE);
 		final String selectedLanguageCode = prefs.getString(MainActivity.SELECTED_LANGUAGE, "en");
@@ -38,21 +44,12 @@ public class MyWidgetProvider extends AppWidgetProvider {
 		final HolidayCalendar holidayCalendar = holidaysDBHelper.getAll(selectedLanguageCode);
 		final HolidayDay holidayDay = holidayCalendar.getTodayHolidays();
 		holidaysDBHelper.close();
-		final String output;
 		if (holidayDay.countHolidays(theme.getBoolean(context.getResources().getString(R.string.settings_usual_holidays), false)) == 0) {
-			output = context.getResources().getString(R.string.no_unusual_holidays);
-		} else {
-			output = holidayDay.getHolidaysList(theme.getBoolean(context.getResources().getString(R.string.settings_usual_holidays), false)).stream()
-					.map(Holiday::getText)
-					.map(text -> context.getResources().getString(R.string.pointed_text, text))
-					.collect(Collectors.joining("\n\n"));
+			return context.getResources().getString(R.string.no_unusual_holidays);
 		}
-		remoteViews.setTextViewText(R.id.widget_text_holiday, output);
-		final PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-		remoteViews.setOnClickPendingIntent(R.id.widget_relative_main, pendingIntent);
-		for (final int i : appWidgetIds) {
-			intent.putExtra("widgetID", i);
-			appWidgetManager.updateAppWidget(i, remoteViews);
-		}
+		return holidayDay.getHolidaysList(theme.getBoolean(context.getResources().getString(R.string.settings_usual_holidays), false)).stream()
+				.map(Holiday::getText)
+				.map(text -> context.getResources().getString(R.string.pointed_text, text))
+				.collect(Collectors.joining("\n\n"));
 	}
 }
