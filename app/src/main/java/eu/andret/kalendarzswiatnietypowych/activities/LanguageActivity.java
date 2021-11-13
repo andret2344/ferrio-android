e10e1287b19b02bf3b5a8124cb68f19d4a43d1f9
Andret2344 <andret2344@gmail.com>
2021-11-13 16:48:47 +0100

    Fixes

--- app/src/main/java/eu/andret/kalendarzswiatnietypowych/activities/LanguageActivity.java
+++ app/src/main/java/eu/andret/kalendarzswiatnietypowych/activities/LanguageActivity.java
@@ -23,6 +23,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+import java.util.TreeSet;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -54,23 +55,26 @@ public class LanguageActivity extends AppCompatActivity {
 				.map(AppCompatActivity::getSupportActionBar)
 				.ifPresent(actionBar -> actionBar.setDisplayHomeAsUpEnabled(true));
 		final HolidaysDBHelper holidaysDBHelper = new HolidaysDBHelper(this);
-		final Set<Language> languages = holidaysDBHelper.getLanguages();
+		final List<Language> databaseLanguages = holidaysDBHelper.getLanguages();
 		holidaysDBHelper.close();
+
+		final AdView adView = findViewById(R.id.language_adview_bottom);
+		adView.loadAd(new AdRequest.Builder().build());
 
 		final ListView listView = findViewById(R.id.language_list_languages);
 		listView.setChoiceMode(AbsListView.CHOICE_MODE_SINGLE);
 		final ExecutorService executorService = Executors.newSingleThreadExecutor();
-		if (Util.isConnection(this)) {
-			CompletableFuture.supplyAsync(new Downloader(), executorService)
-					.thenAccept(languageList -> {
-						languages.addAll(languageList);
-						progressDialog.dismiss();
-					});
+		if (!Util.isConnection(this)) {
+			listView.setAdapter(new LanguageAdapter(this, databaseLanguages));
+			return;
 		}
-		listView.setAdapter(new LanguageAdapter(this, new ArrayList<>(languages)));
-
-		final AdView adView = findViewById(R.id.language_adview_bottom);
-		adView.loadAd(new AdRequest.Builder().build());
+		progressDialog.show();
+		CompletableFuture.supplyAsync(new Downloader(), executorService).thenAccept(languages -> {
+			final Set<Language> languageSet = new TreeSet<>(databaseLanguages);
+			languageSet.addAll(languages);
+			runOnUiThread(() -> listView.setAdapter(new LanguageAdapter(this, new ArrayList<>(languageSet))));
+			progressDialog.dismiss();
+		});
 	}
 
 	@Override
