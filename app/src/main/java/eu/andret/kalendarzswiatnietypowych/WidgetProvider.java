ddadfb526a6dc2220b52761ad19ef3f5fd4340c5
Andret2344 <andret2344@gmail.com>
2023-09-30 21:42:39 +0200

    UHC 3.0 all of a sudden!

--- app/src/main/java/eu/andret/kalendarzswiatnietypowych/WidgetProvider.java
+++ app/src/main/java/eu/andret/kalendarzswiatnietypowych/WidgetProvider.java
@@ -1,17 +1,16 @@
 package eu.andret.kalendarzswiatnietypowych;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.widget.RemoteViews;
 
+import androidx.annotation.NonNull;
 import androidx.preference.PreferenceManager;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
+import java.util.Arrays;
 import java.util.stream.Collectors;
 
 import eu.andret.kalendarzswiatnietypowych.entity.Holiday;
@@ -20,49 +19,28 @@ import eu.andret.kalendarzswiatnietypowych.util.Downloader;
 import java9.util.concurrent.CompletableFuture;
 
 public class WidgetProvider extends AppWidgetProvider {
-	private static final String ACTION_SCHEDULED_UPDATE = "eu.andret.kalendarzswiatnietypowych.SCHEDULED_UPDATE";
-
 	@Override
-	public void onEnabled(final Context context) {
-		super.onEnabled(context);
+	public void onUpdate(@NonNull final Context context, @NonNull final AppWidgetManager appWidgetManager, final int[] appWidgetIds) {
+		final LocalDate now = LocalDate.now();
+		final RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_black);
 
-		final Intent intent = new Intent(ACTION_SCHEDULED_UPDATE);
-		final PendingIntent alarmIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_IMMUTABLE);
-
-		final AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-		final long triggerAtMillis = LocalTime.of(0, 2, 30).toNanoOfDay() / 1000;
-		alarmManager.setInexactRepeating(AlarmManager.RTC, triggerAtMillis, AlarmManager.INTERVAL_DAY, alarmIntent);
+		final Downloader.HolidayDayDownloader downloader = new Downloader.HolidayDayDownloader(now.getMonthValue(), now.getDayOfMonth());
+		CompletableFuture.supplyAsync(downloader)
+				.thenApply(holidayDay -> getContent(context, holidayDay))
+				.thenAccept(text -> remoteViews.setTextViewText(R.id.widget_text_holiday, text))
+				.thenRun(() -> Arrays.stream(appWidgetIds)
+						.forEach(appWidgetId -> appWidgetManager.updateAppWidget(appWidgetId, remoteViews)));
 	}
 
-	@Override
-	public void onDisabled(final Context context) {
-		super.onDisabled(context);
-
-		final Intent intent = new Intent(ACTION_SCHEDULED_UPDATE);
-		final AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-		alarmManager.cancel(PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_IMMUTABLE));
-	}
-
-	@Override
-	public void onReceive(final Context context, final Intent intent) {
-		super.onReceive(context, intent);
-
-		if (ACTION_SCHEDULED_UPDATE.equals(intent.getAction())) {
-			final LocalDate now = LocalDate.now();
-			final RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_black);
-
-			final Downloader.HolidayDayDownloader downloader = new Downloader.HolidayDayDownloader(now.getMonthValue(), now.getDayOfMonth());
-			CompletableFuture.supplyAsync(downloader).thenAccept(holidayDay ->
-					remoteViews.setTextViewText(R.id.widget_text_holiday, getContent(context, holidayDay)));
-		}
-	}
-
-	private String getContent(final Context context, final HolidayDay holidayDay) {
+	@NonNull
+	private String getContent(@NonNull final Context context, @NonNull final HolidayDay holidayDay) {
 		final SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
-		if (holidayDay.countHolidays(preferences.getBoolean(context.getString(R.string.settings_key_usual_holidays), false)) == 0) {
+		final boolean includeUsual = preferences.getBoolean(context.getString(R.string.settings_key_usual_holidays), false);
+		if (holidayDay.countHolidays(includeUsual) == 0) {
 			return context.getString(R.string.no_unusual_holidays);
 		}
-		return holidayDay.getHolidaysList(preferences.getBoolean(context.getString(R.string.settings_key_usual_holidays), false)).stream()
+		return holidayDay.getHolidaysList(includeUsual)
+				.stream()
 				.map(Holiday::getName)
 				.map(text -> context.getString(R.string.pointed_text, text))
 				.collect(Collectors.joining("\n\n"));
