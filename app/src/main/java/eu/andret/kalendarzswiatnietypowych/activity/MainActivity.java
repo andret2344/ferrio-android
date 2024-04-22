0c8410f3de7fb30e9c21e15cc8c4fada253d4bef
Andret2344 <andret2344@gmail.com>
2024-04-22 22:49:58 +0200

    #1: Now the drawer can be closed by the back button

--- app/src/main/java/eu/andret/kalendarzswiatnietypowych/activity/MainActivity.java
+++ app/src/main/java/eu/andret/kalendarzswiatnietypowych/activity/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
@@ -281,6 +282,17 @@ public class MainActivity extends UHCActivity {
 				SettingsActivity.createAlertWithImage(this, R.drawable.holidays, R.string.about_holidays, R.string.about_holidays_text);
 			}
 			return true;
+		});
+
+		getOnBackPressedDispatcher().addCallback(new OnBackPressedCallback(true) {
+			@Override
+			public void handleOnBackPressed() {
+				if (drawer.isOpen()) {
+					drawer.close();
+				} else {
+					finish();
+				}
+			}
 		});
 	}
 }
